07d85fa7aab5557b1724af8d669133726d974336
Paulo José Reis Nunes <paulojrn82@gmail.com>
2022-05-17 15:09:14 -0300

    adicionado componente slider

--- src/app/layout/shared/shared.module.ts
+++ src/app/layout/shared/shared.module.ts
@@ -5,11 +5,13 @@ import { CommonModule } from '@angular/common';
 
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SliderComponent
   ],
   imports: [
     CommonModule,
