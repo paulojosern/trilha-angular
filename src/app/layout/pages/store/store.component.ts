96f879c976ba22503f85f312ba845713fe37a4e1
Carlos Rocha <carlosandrade399@gmail.com>
2022-05-18 13:03:57 -0300

    adiconado games store dinamico

--- src/app/layout/pages/store/store.component.ts
+++ src/app/layout/pages/store/store.component.ts
@@ -1,15 +1,23 @@
+import { GamesService } from './../../../services/games/games.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.scss']
+  styleUrls: ['./store.component.scss'],
 })
 export class StoreComponent implements OnInit {
+  games$: Observable<any>;
+  games: any[];
 
-  constructor() { }
+  constructor(private service: GamesService) {}
 
   ngOnInit(): void {
+    this.games$ = this.service.getGames();
+    this.service.getGames().subscribe((games) => {
+      console.log(games);
+      this.games = games;
+    });
   }
-
 }
