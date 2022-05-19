a89cb19749a0ea87388481ecc7ee89a448e0b8e6
Marcos Boscariolo <marcos.boscariolo@ibm.com>
2022-05-19 17:26:46 -0300

    slider estilizado

--- src/app/layout/shared/carousel/carousel.component.ts
+++ src/app/layout/shared/carousel/carousel.component.ts
@@ -11,10 +11,20 @@ export class CarouselComponent implements OnInit {
 
   @Input() dataGame: IDataCarousel[]
 
+  selectedIndex = 0;
+
   constructor() { }
 
   ngOnInit(): void {
     // console.log(this.dataGame);
+  }
+
+  onNextClick(): void {
+    if(this.selectedIndex === this.dataGame.length - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
   }
 
 }
