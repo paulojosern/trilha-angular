271f787ed48e560657e48d5edf7660b87952864a
Paulo José Reis Nunes <paulojrn82@gmail.com>
2022-05-20 15:44:25 -0300

    Adidionado metodos de filtro

--- src/app/layout/pages/store/store.component.ts
+++ src/app/layout/pages/store/store.component.ts
@@ -10,14 +10,53 @@ import { Component, OnInit } from '@angular/core';
 export class StoreComponent implements OnInit {
   games$: Observable<any>;
   games: any[];
+  gamesBackup: any[];
+  priceList = []
 
-  constructor(private service: GamesService) {}
+  constructor(private service: GamesService) { }
 
   ngOnInit(): void {
     this.games$ = this.service.getGames();
     this.service.getGames().subscribe((games) => {
       console.log(games);
       this.games = games;
+      this.gamesBackup = games;
     });
+  }
+
+
+
+  onChange(value, e): void {
+    e.target.checked ? this.priceList.push(value) :
+      this.priceList = this.priceList.filter(i => i !== value)
+
+    this.games = !this.priceList.length ? (this.games = this.gamesBackup) : this.gamesBackup.filter(i => this.priceList.some(a => a > i.amount))
+  }
+
+  onSortBy(e): void {
+    this.games.sort((a, b) => {
+      if (e) {
+        if (a.amount < b.amount) {
+          return 1;
+        } else if (a.amount > b.amount) {
+          return -1;
+        } else {
+          return 0;
+        }
+      } else {
+        if (a.amount < b.amount) {
+          return -1;
+        } else if (a.amount > b.amount) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    })
+  }
+
+  onSearch(e): void {
+    const result = this.gamesBackup.filter(i => i.title.toLowerCase().includes(e.target.value.toLowerCase()))
+    this.games = !result?.length ? this.gamesBackup : result
   }
 }
