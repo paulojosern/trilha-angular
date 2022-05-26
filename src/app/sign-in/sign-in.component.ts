8d072df50db3426a0a6484b0d083d6662cbc5a28
Laiane <laianep916@gmail.com>
2022-05-26 14:41:02 -0300

    mensagem de erro simples

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,36 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { LoginService } from 'src/app/services/Auth/login.service'
+import { LoginService } from 'src/app/services/Auth/login.service';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-
-  formLogin: FormGroup
+  formLogin: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
     private serviceLogin: LoginService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.formLogin = this.formBuilder.group({
       email: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
   }
 
   onSubmit() {
-    const { email, password } = this.formLogin.value
+    const { email, password } = this.formLogin.value;
     this.serviceLogin.login(email, password).subscribe(
-      result => {
-        console.log(result)
+      (result) => {
+        confirm('ACERTOOU');
       },
-      err => {
-        console.log(err)
+      (err) => {
+        confirm('ERROOOU');
       }
-    )
-
+    );
   }
 }
